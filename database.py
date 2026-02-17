772b73621a8de069a0d23515fde3265cb5fe17fa
domisol132-png <domisol132@gmail.com>
2026-02-17 16:44:58 +0900

    Add files via upload

---
+++ database.py
@@ -0,0 +1,46 @@
+# ==========================================
+# 🗂️ 무적의 합주실 데이터베이스 (위도/경도 좌표 추가!)
+# ==========================================
+STUDIO_DB = {
+    "그라운드합주실 본점": [
+        {"name": "그라운드 본점 S룸", "url": "https://m.booking.naver.com/booking/10/bizes/1061592/items/5587861", "lat": 37.5561, "lon": 126.9234},
+        {"name": "그라운드 본점 A1룸", "url": "https://m.booking.naver.com/booking/10/bizes/1061592/items/5588402", "lat": 37.5561, "lon": 126.9234},
+        {"name": "그라운드 본점 A2룸", "url": "https://m.booking.naver.com/booking/10/bizes/1061592/items/5588476", "lat": 37.5561, "lon": 126.9234},
+        {"name": "그라운드 본점 A3룸", "url": "https://m.booking.naver.com/booking/10/bizes/1061592/items/5588707", "lat": 37.5561, "lon": 126.9234},
+        {"name": "그라운드 본점 B1룸", "url": "https://m.booking.naver.com/booking/10/bizes/1061592/items/5588783", "lat": 37.5561, "lon": 126.9234},
+        {"name": "그라운드 본점 B2룸", "url": "https://m.booking.naver.com/booking/10/bizes/1061592/items/5588817", "lat": 37.5561, "lon": 126.9234},
+        {"name": "그라운드 본점 C룸", "url": "https://m.booking.naver.com/booking/10/bizes/1061592/items/5588835", "lat": 37.5561, "lon": 126.9234},
+        {"name": "그라운드 본점 D룸", "url": "https://m.booking.naver.com/booking/10/bizes/1061592/items/5588869", "lat": 37.5561, "lon": 126.9234},
+        {"name": "그라운드 본점 E룸", "url": "https://m.booking.naver.com/booking/10/bizes/1061592/items/5588887", "lat": 37.5561, "lon": 126.9234}
+    ],
+    "그라운드합주실 홍대1호점": [
+        {"name": "그라운드 홍대1호점 S룸", "url": "https://m.booking.naver.com/booking/10/bizes/329314/items/6651547", "lat": 37.557207, "lon": 126.922592},
+        {"name": "그라운드 홍대1호점 A룸", "url": "https://m.booking.naver.com/booking/10/bizes/329314/items/6651549", "lat": 37.557207, "lon": 126.922592},
+        {"name": "그라운드 홍대1호점 B룸", "url": "https://m.booking.naver.com/booking/10/bizes/329314/items/6651550", "lat": 37.557207, "lon": 126.922592},
+        {"name": "그라운드 홍대1호점 C룸", "url": "https://m.booking.naver.com/booking/10/bizes/329314/items/6651551", "lat": 37.557207, "lon": 126.922592},
+        {"name": "그라운드 홍대1호점 D룸", "url": "https://m.booking.naver.com/booking/10/bizes/329314/items/6651554", "lat": 37.557207, "lon": 126.922592},
+        {"name": "그라운드 홍대1호점 E룸", "url": "https://m.booking.naver.com/booking/10/bizes/329314/items/6646547", "lat": 37.557207, "lon": 126.922592}
+    ],
+    "그라운드합주실 합정1호점": [
+        {"name": "그라운드 합정1호점 S룸", "url": "https://m.booking.naver.com/booking/10/bizes/331813/items/3361583", "lat": 37.550460, "lon": 126.919062},
+        {"name": "그라운드 합정1호점 A룸", "url": "https://m.booking.naver.com/booking/10/bizes/331813/items/3361594", "lat": 37.550460, "lon": 126.919062},
+        {"name": "그라운드 합정1호점 B룸", "url": "https://m.booking.naver.com/booking/10/bizes/331813/items/3361595", "lat": 37.550460, "lon": 126.919062},
+        {"name": "그라운드 합정1호점 C룸", "url": "https://m.booking.naver.com/booking/10/bizes/331813/items/3361597", "lat": 37.550460, "lon": 126.919062}
+    ],
+    "그라운드합주실 신촌1호점": [
+        {"name": "그라운드 신촌1호점 S룸", "url": "https://m.booking.naver.com/booking/10/bizes/1182602/items/5979437", "lat": 37.556237, "lon": 126.940985},
+        {"name": "그라운드 신촌1호점 A룸", "url": "https://m.booking.naver.com/booking/10/bizes/1182602/items/5979448", "lat": 37.556237, "lon": 126.940985},
+        {"name": "그라운드 신촌1호점 B룸", "url": "https://m.booking.naver.com/booking/10/bizes/1182602/items/5979471", "lat": 37.556237, "lon": 126.940985},
+        {"name": "그라운드 신촌1호점 C룸", "url": "https://m.booking.naver.com/booking/10/bizes/1182602/items/5979479", "lat": 37.556237, "lon": 126.940985}
+    ],
+    "제시뮤직 합주실 홍대점": [
+        {"name": "제시뮤직 홍대점 R룸", "url": "https://m.booking.naver.com/booking/10/bizes/446860/items/4013506", "lat": 37.555284, "lon": 126.920221},
+        {"name": "제시뮤직 홍대점 A룸", "url": "https://m.booking.naver.com/booking/10/bizes/446860/items/3702602", "lat": 37.555284, "lon": 126.920221},
+        {"name": "제시뮤직 홍대점 B룸", "url": "https://m.booking.naver.com/booking/10/bizes/446860/items/3702645", "lat": 37.555284, "lon": 126.920221}
+    ],
+    "호랑이합주실": [
+        {"name": "호랑이 1", "url": "https://m.booking.naver.com/booking/10/bizes/1062791/items/5592935", "lat": 37.5575, "lon": 126.9255},
+        {"name": "호랑이 2", "url": "https://m.booking.naver.com/booking/10/bizes/1062791/items/5606830", "lat": 37.5575, "lon": 126.9255},
+        {"name": "호랑이 3", "url": "https://m.booking.naver.com/booking/10/bizes/1062791/items/5606836", "lat": 37.5575, "lon": 126.9255}
+    ]
+}
